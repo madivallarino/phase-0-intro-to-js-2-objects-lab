49eb36c8cb35892357644382d32c41a6055ed2b0
Madison Vallarino <madivallarino@gmail.com>
2021-07-30 17:22:17 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+
+ var employee = { name : 'Madi Vallarino',
+                streetAddress : '113 E 36th Street'}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign({}, employee, {[key]:value});
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign(employee,{[key]:value});
+
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmp = Object.assign({}, employee)
+    delete newEmp[key]
+   return newEmp
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
